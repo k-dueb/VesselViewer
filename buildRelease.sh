891f0c76ecc5ce7466b7aa93e45f1d371f7df42f
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-08-26 07:20:20 -0400

    Added clean.sh and buildrelease.sh

---
+++ buildRelease.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pluginDir=GameData/VesselView/Plugins
+toremove="
+Assembly-CSharp.dll
+Assembly-CSharp-firstpass.dll
+KSPAssets.dll
+KSPCore.dll
+KSPUtil.dll
+Mono.Cecil.dll
+Mono.Security.dll
+RasterPropMonitor.dll
+SaveUpgradePipeline.Core.dll
+System.dll
+System.Xml.dll
+TDx.TDxInput.dll
+Toolbar.dll
+TrackIRUnity.dll
+UnityEngine.dll
+UnityEngine.UI.dll
+Vectrosity.dll
+"
+rm -f  $pluginDir/.xml $pluginDir/.pdb
+for i in $toremove; do
+	rm -f $pluginDir/$i
+done
+
+echo "Enter version: "
+read v
+zip -9r VesselView-$v GameData
